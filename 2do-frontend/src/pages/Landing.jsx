63869ded988c2da031087541e9dd634073a260c9
EmsiPrds <mcvincentparedes@gmail.com>
2025-10-02 09:50:55 +0800

    Style: Create the landing Page design

--- 2do-frontend/src/pages/Landing.jsx
+++ 2do-frontend/src/pages/Landing.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import Logo from "../assets/svg";
 import Threads from "../components/Threads";
 
 export default function LandingPage() {
@@ -21,23 +22,29 @@ export default function LandingPage() {
         <Threads amplitude={5} distance={0} enableMouseInteraction={true} />
       </div>
 
+      <header className="absolute -translate-x-1/2 top-10 left-1/2">
+        <Logo className="h-auto w-36" />
+      </header>
       {/* Main Content */}
       <h1 className="mb-4 text-4xl font-bold text-center md:text-5xl">
-        <span className="text-brand-yellow">2Do</span> App
+        Turn <span className="text-brand-yellow">To-Do's</span> into{" "}
+        <span className="text-brand-yellow">Done!</span>
       </h1>
-      <p className="max-w-md mb-8 text-lg text-center">
-        "Turn <span className="text-brand-yellow">To-Dos</span> into <span className="text-brand-yellow">Done</span>"
+      <p className="max-w-lg mb-8 text-lg text-center">
+        You don't just plan - you{" "}
+        <span className="text-brand-yellow">Make It Happen.</span> Organize
+        beautifully, focus clearly, and achieve more.
       </p>
       <div className="flex flex-col gap-4 lg:flex-row">
         <button
           onClick={() => navigate("/login")}
-          className="px-32 py-2 font-semibold transition rounded-full lg:px-10 bg-brand-yellow text-brand-dark hover:brightness-110"
+          className="px-32 py-1 font-semibold transition border rounded-full lg:px-16 border-brand-light text-brand-light hover:brightness-110"
         >
           Login
         </button>
         <button
           onClick={() => navigate("/signup")}
-          className="px-32 py-2 font-semibold transition border-2 rounded-full lg:px-6 border-brand-yellow text-brand-light hover:bg-brand-yellow hover:text-brand-dark"
+          className="px-32 py-1 font-semibold transition border rounded-full lg:px-16 border-brand-yellow text-brand-yellow hover:brightness-110"
         >
           Sign Up
         </button>
